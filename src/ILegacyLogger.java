972ac2f7ef556d482271f26eafc3b371c522aa87
vlaicuandrei <vlaicuandrei643@gmail.com>
2025-05-04 18:38:11 +0300

    Added ILegacyLogger interface

---
+++ src/ILegacyLogger.java
@@ -0,0 +1,8 @@
+public interface ILegacyLogger {
+
+    void logTheError(String[] log); // used for actually logging the error
+    void setTheLoggingState(boolean state); // used for enabling/disabling the logger
+    int computeTheLogSize(String[] log); // compute the size in bytes of the log message
+    int MAX_LOG_SIZE = 100;
+    boolean isItTooBig(int logSize); // is the log too big to be written?
+}
